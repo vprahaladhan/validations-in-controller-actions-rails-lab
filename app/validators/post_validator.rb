3772dfb3c3158f9a84784eb6f92549951a553d48
prahal <vprahaladhan@gmail.com>
2021-06-09 17:26:45 +1000

    commit tree a5a2cec27bebd4faf4e7bdf5de577acbdfa36cec

---
+++ app/validators/post_validator.rb
@@ -0,0 +1,9 @@
+class PostValidator < ActiveModel::Validator
+
+  def validate(record)
+    unless record.content.length >= 100
+      record.errors[:content] << "Post content should be at least 100 characters long!"
+    end
+  end
+
+end
